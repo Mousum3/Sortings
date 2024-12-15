566378af72bc4219369d3829c6d989b14d5fefb6
Mousum3 <145050412+Mousum3@users.noreply.github.com>
2024-12-15 09:12:07 +0600

    epon

--- Buble_sort.cpp
+++ Buble_sort.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bsort(vector<int> &arr){
-    int n= arr.size();
+void bsort(int arr[], int n){
     for (int i = 0; i < n - 1; i++){
         for (int j = 0; j < n - i - 1; j++){
             if (arr[j] < arr[j + 1]){
@@ -15,8 +14,9 @@ void bsort(vector<int> &arr){
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<int> v={1,5,8,9,7,4,5,6,6,3,2,1,4,7,9,9,6,1,4,5,8,8,7,9,9,6,6,5,5,4,4,8,8,5};
-    bsort(v);
+    int v[]={1,5,8,9,7,4,5,6,6,3,2,1,4,7,9,9,6,1,4,5,8,8,7,9,9,6,6,5,5,4,4,8,8,5};
+    int n = sizeof(v) / sizeof(v[0]);
+    bsort(v,n);
     for(int x: v) cout<<x<<" ";
     return 0;
 }
